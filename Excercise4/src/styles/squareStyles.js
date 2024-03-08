60c96795f73bdb31c55ae69bb9c16104b8f95b82
PARSIVALJ <uchejesusboy@gmail.com>
2024-03-08 19:25:31 +0200

    Using StyleSheet from react native for the styles

--- Excercise4/src/styles/squareStyles.js
+++ Excercise4/src/styles/squareStyles.js
@@ -1,6 +1,7 @@
-export const squareStyles = {
+import { StyleSheet } from 'react-native';
+export const squareStyles = StyleSheet.create({
     square: {
         width: 100,
         height: 100,
     }
-}
+});
